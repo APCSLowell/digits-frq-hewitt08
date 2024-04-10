e63333257f207cf5198f209bb3141ef5363c51c4
hewitt08 <143777083+hewitt08@users.noreply.github.com>
2024-04-09 19:08:20 -0600

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -6,13 +6,23 @@ public class Digits
 	private ArrayList<Integer> digitList;
 
 	public Digits(int num)
-	{ /* to be implemented in part (a) */ 
+	{
+		while(num>9){
+			digitList.add(0, num%10);
+			num=num/10;
+		}
+		digitList.add(0,num);
 	    
 	}
 
 	public boolean isStrictlyIncreasing()
-	{ /* to be implemented in part (b) */
-		
+	{
+		for(int i = 0; i < digitList.size(); i++){
+			if(digitList.get(i-1)>digitList.get(i)){
+				return false
+			}
+		}
+		return true;
 
 	}
 	
